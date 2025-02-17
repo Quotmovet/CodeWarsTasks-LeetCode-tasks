dee1fd389264f974c6eee513e3c456c89a7de1fa
Ruslan <corotn@yandex.ru>
2025-02-17 22:43:56 +0700

    Four tasks

---
+++ src/task3/Task3.kt
@@ -0,0 +1,30 @@
+package task3
+
+import kotlin.math.*
+
+// https://www.codewars.com/kata/5a30e7e9c5e28454790000c1/train/kotlin
+class Task3 {
+
+    private val degrees = mapOf(
+        'A' to 0.0,
+        'B' to 45.0,
+        'C' to 90.0,
+        'D' to 135.0,
+        'E' to 180.0,
+        'F' to 225.0,
+        'G' to 270.0,
+        'H' to 315.0,
+    )
+
+    fun spiderToFly(spider: String, fly: String): Double {
+
+        val r1Value = spider[1].digitToInt()
+        val r2Value = fly[1].digitToInt()
+
+        val diff = abs(degrees.getValue(spider[0]) - degrees.getValue(fly[0]))
+        val deltaTheta = Math.toRadians(min(diff, 360 - diff))
+
+        // Формула Евклидова расстояния в полярных координатах
+        return sqrt(r1Value.toDouble().pow(2) + r2Value.toDouble().pow(2) - 2 * r1Value * r2Value * cos(deltaTheta))
+    }
+}
---
+++ src/task4/Task4.kt
@@ -0,0 +1,42 @@
+package task4
+
+// https://www.codewars.com/kata/55e7280b40e1c4a06d0000aa
+class Task4 {
+
+    fun chooseBestSum(t:Int, k:Int, ls:List<Int>):Int {
+
+        val resulCombinations = combinations(ls, k)
+        val bestValue = maxValueSublist(resulCombinations)
+
+        println(bestValue.filter { it <= t }.max())
+
+        return bestValue.filter { it <= t }.max()
+    }
+
+    // Функция комбинации значений
+    private fun combinations(list: List<Int>, size: Int): List<List<Int>> {
+        if (size > list.size) return emptyList()
+        if (size == 0) return listOf(emptyList())
+
+        return list.flatMapIndexed { index, element ->
+            combinations(list.drop(index + 1), size - 1).map { listOf(element) + it }
+        }
+    }
+
+    /*
+    private fun maxValueSublist(list: List<List<Int>>): List<Int> {
+        val maxValueList = mutableListOf<Int>()
+        for (i in list.indices) {
+            var temp = 0
+            list[i].forEach { value -> temp += value }
+            maxValueList.add(temp)
+        }
+        return maxValueList
+    }
+     */
+
+    private fun maxValueSublist(list: List<List<Int>>): List<Int> {
+        return list.map { it.sum() }
+    }
+
+}
