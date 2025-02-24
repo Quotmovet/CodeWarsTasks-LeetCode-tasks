331790365503d218c3384ad44975e5fd50c5076d
Ruslan <corotn@yandex.ru>
2025-02-24 18:39:37 +0700

    Eighth task

---
+++ src/task8/HumanReadableTime.java
@@ -0,0 +1,13 @@
+package task8;
+
+// https://www.codewars.com/kata/52685f7382004e774f0001f7/java
+public class HumanReadableTime {
+    public static String makeReadable(int seconds) {
+
+        int hoursValue = seconds / 3600;
+        int minutesValue = (seconds % 3600) / 60;
+        int secondsValue = seconds % 60;
+
+        return String.format("%02d:%02d:%02d", hoursValue, minutesValue, secondsValue);
+    }
+}
