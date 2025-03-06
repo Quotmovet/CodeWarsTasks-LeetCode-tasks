579382335b748015616ad3da8e683ad200f92f3c
Ruslan <corotn@yandex.ru>
2025-03-06 22:47:51 +0700

    17 task

---
+++ src/kyu5/kotlin/task17/WeightSortTest.kt
@@ -0,0 +1,15 @@
+package kyu5.kotlin.task17
+
+import org.testng.Assert.assertEquals
+import org.testng.annotations.Test
+
+class WeightSortTest {
+
+    @Test
+    fun BasicTests() {
+        println("****** Basic Tests ******")
+        assertEquals("2000 103 123 4444 99", orderWeight("103 123 4444 99 2000"))
+        assertEquals("11 11 2000 10003 22 123 1234000 44444444 9999", orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"))
+
+    }
+}
