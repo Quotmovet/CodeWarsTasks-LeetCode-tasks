da2ee69c6e2422644a1e07f31675c009e5ba2bc3
Ruslan <corotn@yandex.ru>
2025-03-07 22:59:58 +0700

    18 task

---
+++ src/kyu5/java/task18/Kata.java
@@ -0,0 +1,28 @@
+package kyu5.java.task18;
+
+// https://www.codewars.com/kata/530e15517bc88ac656000716/
+public class Kata {
+    public static String rot13(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                char shifted = (char) (c + 13);
+                if (shifted > 'z') {
+                    shifted = (char) (c - 13);
+                }
+                result.append(shifted);
+            } else if (c >= 'A' && c <= 'Z') {
+                char shifted = (char) (c + 13);
+                if (shifted > 'Z') {
+                    shifted = (char) (c - 13);
+                }
+                result.append(shifted);
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+}
